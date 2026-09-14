1b3eb45c6720e58f52ff669814aeaf8e9b882f28
Annaev Serdar <ansernps@gmail.com>
2026-09-15 00:42:24 +0300

    превратил все в комменты

--- feature.py
+++ feature.py
@@ -1,6 +1,4 @@
-feature file
-but there is no feature
-8======================D
-
-no matter what i breed
-we still are made of seed
+# but there is no feature
+# 8======================D
+# no matter what i breed
+# we still are made of seed
